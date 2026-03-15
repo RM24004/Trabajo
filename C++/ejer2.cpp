8a09132a10909140f783998d1aef9ee4de2e4dac
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-03-15 16:10:31 -0600

    Reparacion de la resta, ademas se agrego la division

--- C++/ejer2.cpp
+++ C++/ejer2.cpp
@@ -3,22 +3,31 @@
 using namespace std;
 
 int main() {
-    const int SIZE = 5;
+    const int SIZE = 3;
     int vector[SIZE];
     int suma = 0;
     int resta = 0;
     int multiplicacion = 1;
+    double division = 1.0;
     double mediaAritmetica;
 
-    string encabezados[4] = {"Suma", "Resta", "Multiplicacion", "Media Aritmetica"};
-    double resultados[4];
+    string encabezados[5] = {"Suma", "Resta", "Multiplicacion", "Division", "Media Aritmetica"};
+    double resultados[5];
 
     cout << "Ingrese los elementos del vector:" << endl;
     for (int i = 0; i < SIZE; i++) {
         cin >> vector[i];
         suma += vector[i];
-        resta -= vector[i];
         multiplicacion *= vector[i];
+        if (i == 0) {
+            resta = vector[i];
+            division = vector[i];
+        } else {
+            resta -= vector[i];
+            division /= vector[i];
+        }
+        
+        
     }
 
     mediaAritmetica = static_cast<double>(suma) / SIZE;
@@ -26,11 +35,12 @@ int main() {
     resultados[0] = suma;
     resultados[1] = resta;
     resultados[2] = multiplicacion;
-    resultados[3] = mediaAritmetica;
+    resultados[3] = division;
+    resultados[4] = mediaAritmetica;
 
     cout << endl;
     // Mostrar encabezados y resultados
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << encabezados[i] << ": " << resultados[i] << endl;
     }
     
